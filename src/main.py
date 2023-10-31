fcfb23ae08cb96915e31010cd98d7ea9040e50aa
João Victor <joao15victor08@gmail.com>
2023-10-31 10:33:09 -0300

    Adiciona cors middleware e rota de comentarios

--- src/main.py
+++ src/main.py
@@ -1,13 +1,29 @@
 import uvicorn
 from fastapi import FastAPI
+from controller import commentController
+from database import SessionLocal, engine
+from model import commentModel
+from fastapi.middleware.cors import CORSMiddleware
+
+commentModel.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
 
 @app.get("/")
 async def root():
     return {"message": "Hello from Video Service"}
 
+
+app.include_router(prefix="/api", router=commentController.comment)
 
 if __name__ == '__main__':
   uvicorn.run('main:app', reload=True)
