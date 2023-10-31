2d8038c438821da0ea96fa53936494792445d5b6
João Victor <joao15victor08@gmail.com>
2023-10-31 10:32:02 -0300

    Adiciona rota para criar comentario

--- src/controller/commentController.py
+++ src/controller/commentController.py
@@ -9,10 +9,15 @@ comment = APIRouter(
   prefix="/comments"
 )
 
-@comment.get("/{video_id}", response_model=commentSchema.Comment)
+
+@comment.get("/{video_id}", response_model=list[commentSchema.Comment])
 def read_comment(video_id: int, db: Session = Depends(get_db)):
-  comment = commentRepository.get_comment_by_id(db, video_id)
+  comment = commentRepository.get_comment_by_id(db, video_id=video_id)
   if not comment:
     raise HTTPException(status_code=404, detail="erro")
   return comment
 
+@comment.post("/", response_model=commentSchema.Comment)
+def create_comment(comment: commentSchema.CommentCreate, db: Session = Depends(get_db)):
+  print(comment)
+  return commentRepository.create_comment(db=db, video_id=comment.video_id, user_id=comment.user_id, content=comment.content)
