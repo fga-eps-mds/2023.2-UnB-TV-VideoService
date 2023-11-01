6a40579a1ccc84d2719052d98efffa9a80f16871
Marcos Castilhos <221008300@aluno.unb.br>
2023-11-01 16:11:32 -0300

    adiciona mensagens de erro e função delete
    Co-authored-by: João Victor <joao15victor08@gmail.com>

--- src/controller/commentController.py
+++ src/controller/commentController.py
@@ -4,20 +4,27 @@ from sqlalchemy.orm import Session
 
 from domain import commentSchema
 from repository import commentRepository
+from constants import errorMessages
 
 comment = APIRouter(
   prefix="/comments"
 )
 
-
 @comment.get("/{video_id}", response_model=list[commentSchema.Comment])
 def read_comment(video_id: int, db: Session = Depends(get_db)):
-  comment = commentRepository.get_comment_by_id(db, video_id=video_id)
-  if not comment:
-    raise HTTPException(status_code=404, detail="erro")
+  comment = commentRepository.get_comments_by_video_id(db, video_id=video_id)
   return comment
 
 @comment.post("/", response_model=commentSchema.Comment)
 def create_comment(comment: commentSchema.CommentCreate, db: Session = Depends(get_db)):
-  print(comment)
-  return commentRepository.create_comment(db=db, video_id=comment.video_id, user_id=comment.user_id, content=comment.content)
+  return commentRepository.create_comment(db=db, video_id=comment.video_id, 
+  user_id=comment.user_id, content=comment.content)
+
+@comment.delete("/{id}", response_model=commentSchema.Comment)
+def delete_comment(id: int, db: Session = Depends(get_db)):
+  comment = commentRepository.get_comment_by_id(db, id)
+  if not comment:
+    raise HTTPException(status_code=404, detail=errorMessages.COMMENT_NOT_FOUND)
+  commentRepository.delete_comment(db,comment)
+  return comment
+
